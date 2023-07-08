f53ae6c469f6c47a3fcae1926413fc84571e94d0
JM Tiong <jmtiong92@gmail.com>
2023-07-09 02:58:59 +0800

    added essential module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './util/prisma.service';
+import { SchedulerModule } from './scheduler/schedule.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { EssentialModule } from './util/essential.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), EssentialModule, ScheduleModule.forRoot(), SchedulerModule],
   controllers: [AppController],
-  providers: [PrismaService, AppService],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/util/essential.module.ts
+++ backend/src/util/essential.module.ts
@@ -1,20 +1,10 @@
-import { HttpModule } from "@nestjs/axios";
 import { Global, Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { HttpClientService } from "./http-client.service";
 import { DatetimeService } from "./date-time.service";
 import { PrismaService } from "./prisma.service";
 
 @Global()
 @Module({
-  imports: [
-    HttpModule.registerAsync({
-      imports: [ConfigModule],
-      useExisting: HttpClientService
-    })
-  ],
-  exports: [
-    HttpClientService, DatetimeService, PrismaService
-  ]
+  providers: [ DatetimeService, PrismaService ],
+  exports: [ DatetimeService, PrismaService ]
 })
 export class EssentialModule {}
