abd5aa3957f6d4a68a0309309afc27c8efc28101
JM Tiong <jmtiong92@gmail.com>
2023-07-14 23:28:30 +0800

    added a maincontext for all component to utilize

---
+++ frontend/src/context/MainContext.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react"
+import { AreaWithWeatherDto, CameraDto, TrafficCaptureDto } from "../services/openapi"
+import { createContext } from "use-context-selector"
+
+export type SwatModelType = {
+  areaWeathers: AreaWithWeatherDto[]
+  setAreaWeathers: React.Dispatch<React.SetStateAction<AreaWithWeatherDto[]>>
+  currentSelectedArea: AreaWithWeatherDto
+  setCurrentSelectedArea: React.Dispatch<React.SetStateAction<AreaWithWeatherDto>>
+  filteredAreas: AreaWithWeatherDto[]
+  setFilteredAreas: React.Dispatch<React.SetStateAction<AreaWithWeatherDto[]>>
+  cameras: CameraDto[]
+  setCameras: React.Dispatch<React.SetStateAction<CameraDto[]>>
+  currentCamera: CameraDto
+  setCurrentCamera: React.Dispatch<React.SetStateAction<CameraDto>>
+  trafficCapture: TrafficCaptureDto
+  setTrafficCapture: React.Dispatch<React.SetStateAction<TrafficCaptureDto>>
+}
+
+export const SwatContext = createContext<SwatModelType>({} as SwatModelType)
+
+export type WTContext = {
+  children: React.ReactNode
+};
+
+const MainContext = ({children}: WTContext) => {
+  const [areaWeathers, setAreaWeathers] = useState<AreaWithWeatherDto[]>([])
+  const [cameras, setCameras] = useState<CameraDto[]>([])
+  const [filteredAreas, setFilteredAreas] = useState<AreaWithWeatherDto[]>([])
+  const [currentSelectedArea, setCurrentSelectedArea] = useState<AreaWithWeatherDto>({} as AreaWithWeatherDto)
+  const [currentCamera, setCurrentCamera] = useState<CameraDto>({} as CameraDto)
+  const [trafficCapture, setTrafficCapture] = useState<TrafficCaptureDto>({} as TrafficCaptureDto)
+
+  return (
+    <SwatContext.Provider value={{
+      areaWeathers, setAreaWeathers,
+      cameras, setCameras,
+      filteredAreas, setFilteredAreas,
+      currentSelectedArea, setCurrentSelectedArea,
+      currentCamera, setCurrentCamera,
+      trafficCapture, setTrafficCapture
+    }}>
+      { children }
+    </SwatContext.Provider>
+  );
+}
+
+export default MainContext;
