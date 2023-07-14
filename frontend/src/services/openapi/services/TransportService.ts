647ad8dd2cb2445499dc98e02ce9c4c339621842
JM Tiong <jmtiong92@gmail.com>
2023-07-14 23:27:36 +0800

    added openapi codegen code

---
+++ frontend/src/services/openapi/services/TransportService.ts
@@ -0,0 +1,48 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { CameraDto } from '../models/CameraDto';
+import type { TrafficCaptureDto } from '../models/TrafficCaptureDto';
+import type { TrafficRequestDto } from '../models/TrafficRequestDto';
+
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+
+export class TransportService {
+
+    /**
+     * @param requestBody
+     * @returns TrafficCaptureDto
+     * @throws ApiError
+     */
+    public static retrieveTrafficCapture(
+        requestBody: TrafficRequestDto,
+    ): CancelablePromise<TrafficCaptureDto> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/v1/transport/image',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+
+    /**
+     * @param area
+     * @returns CameraDto
+     * @throws ApiError
+     */
+    public static retrieveListOfCameras(
+        area: string,
+    ): CancelablePromise<Array<CameraDto>> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/v1/transport/cameras',
+            query: {
+                'area': area,
+            },
+        });
+    }
+
+}
