bbf9d712090dfe7f52d2fb8b270cd4767ecb533b
JM Tiong <jmtiong92@gmail.com>
2023-07-16 02:01:39 +0800

    Updated date picker to use context state

--- frontend/src/components/DateTimeToggle.tsx
+++ frontend/src/components/DateTimeToggle.tsx
@@ -1,28 +1,32 @@
-import { Col, DatePicker, Row, Switch } from "antd";
-import { FC, useState } from "react";
+import { Col, DatePicker, DatePickerProps, Row } from "antd";
+import { FC } from "react";
+import { useContextSelector } from "use-context-selector";
+import { SwatContext } from "../context/MainContext";
 
 export type DatePickerType = 'Date Time' | 'Date'
 
 const DateTimeToggle: FC = () => {
-  const [toggle, setToggle] = useState<DatePickerType>('Date Time')
+  const { datetime, setDatetime } = useContextSelector(SwatContext, ({datetime, setDatetime}) => { return { datetime, setDatetime }})
 
-  const changeDatePicker = (checked: boolean) => {
-    setToggle(checked ? 'Date Time' : 'Date')
-  }
-
+  const onChange = (
+    value: DatePickerProps['value'],
+    dateString: [string, string] | string,
+  ) => {
+    if (value?.unix()) {
+      setDatetime(value?.unix()*1000)
+    } else {
+      setDatetime(undefined)
+    }
+  };
   return (
     <>
-      {/* <Row>
-        <Col span={24}>
-          <Switch defaultChecked onChange={changeDatePicker} checkedChildren="Date Time" unCheckedChildren="Date"></Switch>
-        </Col>
-      </Row> */}
       <Row style={{ padding: '10px'}}>
         <Col span={24}>
           <DatePicker
-            placeholder={toggle === 'Date Time' ? 'Select Date Time' : 'Select Date'}
+            placeholder='Select Date Time'
             style={{ width: '100%' }}
-            showTime={toggle === 'Date Time'}
+            showTime={true}
+            onChange={onChange}
           ></DatePicker>
         </Col>
       </Row>
